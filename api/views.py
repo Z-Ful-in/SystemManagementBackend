7f047b8fb692bd9c937af45eeb3bb4146e48e067
ZIHIKA <1918938521@qq.com>
2025-04-18 17:21:13 +0800

    Login and Logout

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
@@ -33,3 +34,45 @@ def register_view(request):
                 'code': 400
             }
         }))
+
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+        login(request, user)
+        return JsonResponse(
+            AuthResponseSerializer({
+                'success': True,
+                'message':{
+                    'message': 'login successfully',
+                    'code': 201
+                }
+            })
+        )
+    else:
+        return JsonResponse(
+            AuthResponseSerializer({
+                'success': False,
+                'message':{
+                    'message': 'username or password is incorrect',
+                    'code': 401
+                }
+            })
+        )
+
+
+@api_view(['POST'])
+def logout_view(request):
+    logout(request)
+    return JsonResponse(
+        AuthResponseSerializer({
+            'success': True,
+            'message':{
+                'message': 'logout successfully',
+                'code': 202
+            }
+        })
+    )
